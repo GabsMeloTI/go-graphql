fcbcb34f23fe5f5df661ca9a8376afc461a560b0
GabsMeloTI <rm550202@fiap.com.br>
2025-01-13 13:44:52 -0300

    feat: add cors.

--- cmd/api.go
+++ cmd/api.go
@@ -2,14 +2,23 @@ package cmd
 
 import (
 	"context"
+	"github.com/99designs/gqlgen/handler"
 	"github.com/GabsMeloTI/go-graphql/infra"
+	"github.com/GabsMeloTI/go-graphql/internal/generated"
+	"github.com/GabsMeloTI/go-graphql/internal/resolvers"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"time"
 )
 
 func StartAPI(ctx context.Context, container *infra.ContainerDI) {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.OPTIONS},
+	}))
 
 	go func() {
 		for {
@@ -26,6 +35,7 @@ func StartAPI(ctx context.Context, container *infra.ContainerDI) {
 	}()
 
 	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+	e.POST("/query", echo.WrapHandler(handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{DB: container.ConnDB}}))))
 
 	e.Logger.Fatal(e.Start(container.Config.ServerPort))
 }
